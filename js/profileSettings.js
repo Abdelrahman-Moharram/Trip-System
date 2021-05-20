1ac4de5769b5b67b3148162653638a2cd0bbb7d2
Abdelrahman-Moharram <abd0m07arram@yahoo.com>
2021-05-20 21:33:16 +0200

    add javascript to profile page to can switch tabs and fix bugs

---
+++ js/profileSettings.js
@@ -0,0 +1,12 @@
+function switchProfileSetting(settid,conid) {
+    var settings = document.getElementsByClassName("single-setting-cont");
+    var profilecons = document.getElementsByClassName("user-info-container");
+    var i,j;
+    for(i = 0; i < 4; i++){
+        settings[i].classList.remove("active-profile-settings");
+        profilecons[i].style.display = "none";
+    }
+    document.getElementById(settid).className += " active-profile-settings";
+    document.getElementById(conid).style.display = "block";
+
+}
